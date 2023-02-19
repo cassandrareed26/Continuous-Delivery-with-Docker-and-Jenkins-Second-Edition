4948b1855bbb458faa1c23b6ce55f1341b35aba5
cassandrareed26 <113269449+cassandrareed26@users.noreply.github.com>
2023-02-19 15:10:41 -0500

    Update Calculator.java
    Trying to add a javadoc comment

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -2,7 +2,11 @@ package com.leszko.calculator;
 
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
-
+/**
+ *
+ * @author CReed
+ *
+ */
 @Service
 public class Calculator {
         final static int umlNUMBER1 = 3;
